d3db218d38e66244375f24cbcb849b8e9308a9f6
malin <malin3598@foxmail.com>
2023-11-06 15:36:48 +0800

    添加条形图,关系图

---
+++ bar_column.js
@@ -0,0 +1,134 @@
+var ROOT_PATH = '';
+
+var chartDom = document.getElementById('main');
+var myChart = echarts.init(chartDom);
+var option;
+fetch(ROOT_PATH+'/repo_dependencies/top_referenced_file')
+    .then(response => {
+        // console.log(response)
+        return response.json()
+    })
+    .then(data => {
+        // console.log(data)
+        // 数据成功返回后，填充下拉框的选项
+        data.forEach(option => {
+            const optionElement = document.createElement('option');
+            console.log(option['file_path'])
+            optionElement.value = option['file_path']
+            // optionElement.value = option.value;
+            optionElement.text = option['file_path'];
+            dropdown.appendChild(optionElement);
+        });
+    })
+    .catch(error => {
+        console.error('获取选项数据失败:', error);
+    });
+dropdown.addEventListener('change', function() {
+    const selectedValue = dropdown.value;
+    console.log('选择的值:', selectedValue);
+    myChart.hideLoading();
+    let option_data
+    fetch(ROOT_PATH+'/repo_dependencies/top_referenced_file_area_commit_info?value='+selectedValue, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json', // 根据你的需求设置头部
+            // 其他头部参数
+        },
+
+    })
+        .then(response => response.json())
+        .then(data => {
+            // option_data = data
+            console.log('-----------------------')
+            console.log(data); // 处理后端返回的数据
+            console.log('..................')
+            option = {
+                tooltip: {
+                    trigger: 'axis',
+                    axisPointer: {
+                        type: 'shadow',
+                        label: {
+                            show: true
+                        }
+                    }
+                },
+                toolbox: {
+                    show: true,
+                    feature: {
+                        mark: { show: true },
+                        dataView: { show: true, readOnly: false },
+                        magicType: { show: true, type: ['line', 'bar'] },
+                        restore: { show: true },
+                        saveAsImage: { show: true }
+                    }
+                },
+                calculable: true,
+                legend: {
+                    data: ['0时区', '中国', '北美', '印度', '日韩', '欧洲', '澳洲'],
+                    itemGap: 10,
+                    width: '100%',
+                    right: '25%'
+                },
+                grid: {
+                    top: '12%',
+                    left: '3%',
+                    right: '10%',
+                    width: '90%',
+                    containLabel: true
+                },
+                xAxis: [
+                    {
+                        type: 'category',
+                        data: data['years']
+                    }
+                ],
+                yAxis: [
+                    {
+                        type: 'value',
+                        name: 'alter_file_lines',
+                        // axisLabel: {
+                        //     formatter: function (a) {
+                        //         a = +a;
+                        //         return isFinite(a) ? echarts_sample.format.addCommas(+a / 1000) : '';
+                        //     }
+                        // }
+                    }
+                ],
+                dataZoom: [
+                    {
+                        show: true,
+                        start: 94,
+                        end: 100
+                    },
+                    {
+                        type: 'inside',
+                        start: 94,
+                        end: 100
+                    },
+                    {
+                        show: true,
+                        yAxisIndex: 0,
+                        filterMode: 'empty',
+                        width: 30,
+                        height: '80%',
+                        showDataShadow: false,
+                        left: '93%'
+                    }
+                ],
+                series: data['series']
+            };
+            myChart.setOption(option);
+
+        })
+        .catch(error => {
+            console.error('请求错误:', error);
+        });
+
+
+
+});
+option && myChart.setOption(option);
+
+
+
+
---
+++ graph.js
@@ -0,0 +1,82 @@
+
+
+var ROOT_PATH = '';
+var api_path = '/repo_dependencies/one_file_reference_graph'
+var chartDom = document.getElementById('graph');
+var myChart = echarts.init(chartDom);
+var option;
+const dropdown = document.getElementById('dropdown');
+myChart.showLoading();
+
+dropdown.addEventListener('change', function() {
+    const selectedValue = dropdown.value;
+    // console.log('选择的值:', selectedValue);
+    // myChart.hideLoading();
+    fetch(ROOT_PATH+ api_path+'?'+'file='+selectedValue, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json', // 根据你的需求设置头部
+            // 其他头部参数
+        },
+
+    })
+        .then(response => response.json())
+        .then(graph => {
+            myChart.hideLoading();
+            console.log(graph)
+            graph.nodes.forEach(function (node) {
+                node.label = {
+                    show: node.symbolSize > 30
+                };
+            });
+            option = {
+                title: {
+                    text: '文件引用关系图',
+                    subtext: 'Circular layout',
+                    top: 'bottom',
+                    left: 'right'
+                },
+                tooltip: {},
+                legend: [
+                    {
+                        data: graph.categories.map(function (a) {
+                            return a.name;
+                        })
+                    }
+                ],
+                animationDurationUpdate: 1500,
+                animationEasingUpdate: 'quinticInOut',
+                series: [
+                    {
+                        name: 'file',
+                        type: 'graph',
+                        layout: 'circular',
+                        circular: {
+                            rotateLabel: true
+                        },
+                        data: graph.nodes,
+                        links: graph.links,
+                        categories: graph.categories,
+                        roam: true,
+                        label: {
+                            position: 'right',
+                            formatter: '{b}'
+                        },
+                        lineStyle: {
+                            color: 'source',
+                            curveness: 0.3
+                        }
+                    }
+                ]
+            };
+            myChart.setOption(option);
+
+        })
+        .catch(error => {
+            console.error('请求错误:', error);
+        });
+
+
+
+});
+option && myChart.setOption(option);
